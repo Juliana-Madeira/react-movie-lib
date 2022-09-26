36f22b8fcae66a0620683c8fda9d04a73e152a8d
Juliana Madeira <juliwood1980@gmail.com>
2022-09-26 17:08:47 -0300

    feat: new carousel and home changes

---
+++ src/components/CarouselMovies/CarouselMovies.js
@@ -0,0 +1,45 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import React from 'react';
+
+
+import singin_rain from  '../../assets/images/singin_rain.png';
+import leiDaNoite from '../../assets/images/leidanoite.jpg';
+import pulpFiction from '../../assets/images/pulp-fiction.jpg';
+import legadoMarvel from '../../assets/images/llegadomarvel.jpg';
+import starWars from '../../assets/images/starwars.png';
+
+const CarouselMovies = () => {
+  return (
+    <div>
+        <Carousel 
+            autoPlay={true} 
+            infiniteLoop={true} 
+            interval={6000} 
+            stopOnHover={true} 
+            transitionTime={2000}
+            showStatus={false}
+            showIndicators={false}
+            showThumbs={false}
+        >
+            <div>
+                <img src={singin_rain} alt='singinrain' />
+            </div>
+            <div>
+                <img src={pulpFiction} alt="pulpfitction" />
+            </div>
+            <div>
+                <img src={leiDaNoite} alt="leiDaNoite" />
+            </div>
+            <div>
+                <img src={legadoMarvel} alt="legadoMarvel" />
+            </div>
+            <div>
+                <img src={starWars} alt="poderosoChefao" />
+            </div>
+        </Carousel>
+    </div>
+  )
+}
+
+export default CarouselMovies;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { FaAngleDown } from "react-icons/fa";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 
-import CarouselBrands from '../../components/CarouselBrands/CarouselBrands';
+import CarouselMovies from '../../components/CarouselMovies/CarouselMovies';
 
 import '../Home/Home.css';
 
@@ -14,17 +15,28 @@ const Home = () => {
     navigate('/popular-movies')
   }
 
+  const handleToButtonsHome = () => {
+    navigate('#btn')
+  }
+
   return (
-    <div className='home_container'>
-      <p className='title'>Welcome to the best MovieLib!</p>
-      <CarouselBrands/>
+    <div className='home_container' >
+      <p className='title' id='#top'>Welcome to the best MovieLib!</p>
+      <CarouselMovies/>
       <div className='categories'>
         <p className='title_categories'>Categories</p>
         <button type="button" className="faAngleDown_button">
-          <FaAngleDown size={25}/>
+          <HashLink to="#btn">
+            <FaAngleDown size={25}/>
+          </HashLink>
         </button>
       </div>
-      <div  className='buttons_group'>
+      <div  className='buttons_group' id='btn'>
+        <HashLink to="#top">
+          <button className='home_button'>
+            Home
+          </button>
+        </HashLink>
         <button className='home_button' onClick={handleToPopularMovies}>Popular Movies</button>
         <button className='home_button'>Top Rated Movies</button>
         <button className='home_button'>People</button>
